ed6f1abf05724df9426121566339d9b24b16a2ee
eric <90855552+44w@users.noreply.github.com>
2023-03-30 14:57:16 -0400

    Add files via upload

---
+++ site.config.js
@@ -0,0 +1,70 @@
+const CONFIG = {
+  // profile setting (required)
+  profile: {
+    name: "eric // @hillsto35th",
+    image: "/avatar.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "High School Student",
+    bio: "Average Cybersecurity Student",
+    email: "eric@35th.io",
+    linkedin: "ejohnson06",
+    github: "44w",
+    instagram: "hillsto35th",
+  },
+  projects: [
+    {
+      name: `44w.github/io`,
+      href: "https://github.com/44w/44w.github.io",
+    },
+  ],
+  // blog setting (required)
+  blog: {
+    title: "eric // 35th.io ",
+    description: "welcome to my blog ^__^",
+    theme: "dark", // ['light', 'dark', 'auto']
+  },
+
+  // CONFIG configration (required)
+  link: "https://morethan-log.vercel.app",
+  since: 2023, // If leave this empty, current year will be used.
+  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+  seo: {
+    keywords: ["Blog", "Website", "Notion"],
+  },
+
+  // notion configuration (required)
+  notionConfig: {
+    pageId: "6b0225ccc90d4fd4a7cd351d403b13e8",
+  },
+
+  // plugin configuration (optional)
+  googleAnalytics: {
+    enable: false,
+    config: {
+      measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
+    },
+  },
+  googleSearchConsole: {
+    enable: false,
+    config: {
+      siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
+    },
+  },
+  utterances: {
+    enable: false,
+    config: {
+      repo: "morethanmin/morethan-log",
+      "issue-term": "og:title",
+      label: "💬 Utterances",
+    },
+  },
+  cusdis: {
+    enable: false,
+    config: {
+      host: "https://cusdis.com",
+      appid: "", // Embed Code -> data-app-id value
+    },
+  },
+  isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+}
+module.exports = CONFIG
